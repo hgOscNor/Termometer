dd12ccc44272c3efa583458880d97f07e2cf0c49
hgOscNor <oscar.noring@hitachigymnasiet.se>
2024-11-19 16:09:00 +0100

    added base html, js and css

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// Din Firebase-konfiguration (från Firebase Console)
+const firebaseConfig = {
+    apiKey: "AIzaSyCQDevcOGsNzQTy0F63KP5b3DJQOjmH3jk",
+    authDomain: "i-hetaste-laget-ead3c.firebaseapp.com",
+    databaseURL: "https://i-hetaste-laget-ead3c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "i-hetaste-laget-ead3c",
+    storageBucket: "i-hetaste-laget-ead3c.firebasestorage.app",
+    messagingSenderId: "96796478648",
+    appId: "1:96796478648:web:3ae32b69593d060c97b84d"
+};
+
+// Initiera Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+
+let hum = db.ref("sensor.HumAVG")
+
+console.log(hum)
