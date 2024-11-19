c0621dc6a197cdfd21e8fa834df0a5855113f5f9
hgOscNor <oscar.noring@hitachigymnasiet.se>
2024-11-19 14:40:43 +0100

    arduino code v1.0a done

--- arduinoCode/Tempsensor/Tempsensor.ino
+++ arduinoCode/Tempsensor/Tempsensor.ino
@@ -21,7 +21,7 @@ AM2320 sensor;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define timeBetweenUploads 10
-#define NUM_SAMPLES 10 // Number of values when calculating average
+#define NUM_SAMPLES 60 // Number of values when calculating average
 float SensorTemp;
 float SensorHum;
 float Temp;
